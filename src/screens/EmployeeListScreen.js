75fc453f510d087194f0240ee15205cf22b0b4f3
Venkatesh <venkateshnalla94@gmail.com>
2020-11-06 12:23:23 +0530

    Welcome text through context.

--- src/screens/EmployeeListScreen.js
+++ src/screens/EmployeeListScreen.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import ListItem from '../components/ListItem';
 import * as data from '../data/employeInfo';
+import {Context as AuthContext} from '../context/AuthContext';
 
 const EmployeeListScree=()=>{
+     const {state} = useContext(AuthContext);
      return(
-          <View>
-               
+          <View style={EmployeeListScreeStyles.mainView}>
+               <Text style={EmployeeListScreeStyles.welcomeText}>Welcome {state.userName} </Text>
                <FlatList
                     data={data.default.user}
                     keyExtractor={(data)=>{return data.email}}
                     renderItem={({item})=>{
                          return <ListItem data={item}/>
                     }}
+                   
                />
           </View>
      )
 }
+const EmployeeListScreeStyles=StyleSheet.create({
+     mainView:{
+          flex:1,
+     },
+     welcomeText:{
+          fontSize:25,
+          marginHorizontal:50,
+          marginTop:60,
+          textAlign:'center',
+     }
+})
 export default EmployeeListScree;
