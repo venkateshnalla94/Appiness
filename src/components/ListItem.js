81d07dd564c19259e66e3bd989d0307a7e6937c9
Venkatesh <venkateshnalla94@gmail.com>
2020-11-07 15:35:31 +0530

    Design changes has been added to the ListItem.js and EmployeeListScreen.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,58 +1,70 @@
-import React, {useContext} from 'react';
-import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const profilePic={uri:'https://png.pngtree.com/png-vector/20190429/ourmid/pngtree-employee-icon-vector-illustration-in-glyph-style-for-any-purpose-png-image_998315.jpg'}
 
 const ListItem=({data})=>{
      
      return(
-          <View style={styles.parentView}>
-               <View style={styles.profile}>
-                    <Image
-                         source={profilePic}
-                         style={styles.userImage}
-                    ></Image>
-               </View>
+          <TouchableOpacity
+          onPress={()=>{
+               console.log("Will provide More information about the  user"+data.name)
                
-               <View style={styles.primaryChild}>
-                    <Text style={styles.employeeName}>{data.name.charAt(0).toUpperCase()+data.name.slice(1)}</Text>
-                    <View style={styles.ageAndGender}>
-                         <Text style={styles.ageAndGenderText}>Age = {data.age}</Text>
-                         <Text style={styles.ageAndGenderText}>Gender = {data.gender}</Text>
+          }}
+          >
+               <View style={styles.parentView}>
+                    <View style={styles.profile}>
+                         <Image
+                              source={profilePic}
+                              style={styles.userImage}
+                         ></Image>
                     </View>
-                    <View style={styles.emailAndPhone}>
-                         <Text style={styles.email}>{data.email}</Text>
-                         <Text style={styles.ageAndGenderText}>{data.phoneNo}</Text>
+                    <View style={styles.primaryChild}>
+                         <Text style={styles.employeeName}>{data.name.charAt(0).toUpperCase()+data.name.slice(1)}</Text>
+                         <View style={styles.ageAndGender}>
+                              <Text style={styles.ageAndGenderText}>Age = {data.age}</Text>
+                              <Text style={styles.ageAndGenderText}>Gender = {data.gender}</Text>
+                         </View>
+                         <View style={styles.emailAndPhone}>
+                              <Text style={styles.email}>{data.email}</Text>
+                              <Text style={styles.email}>{data.phoneNo}</Text>
+                         </View>
                     </View>
                </View>
-          </View>
+     
+          </TouchableOpacity>
+          
      )
 }
 
 const styles=StyleSheet.create({
      parentView:{
+          
           flexDirection:'row',
           marginHorizontal:30,
           marginTop:20,
      },
      profile:{
-          flex:.4,
+          flex:.35,
           justifyContent:"center",
-          backgroundColor:"gold",
+          //backgroundColor:"black",
           paddingRight:5
      },
      primaryChild:{
           flex:.75,
-          borderWidth:5,
-          borderColor:'gold',
+          //borderWidth:5,
+          //borderColor:'gold',
+          //paddingLeft:5
           
      },
      employeeName:{
           flexDirection: 'row',
           padding:5,
           fontWeight:'bold',
-          fontFamily: "Telugu Sangam MN",
+          fontFamily: "Helvetica Neue",
           fontSize:22,
+          color:'#f5f5f5',
+          paddingTop:10
      },
      ageAndGender:{
           flexDirection: 'row',
@@ -62,21 +74,26 @@ const styles=StyleSheet.create({
      },
      ageAndGenderText:{
           fontSize:18,
-          fontWeight: '200'
+          fontWeight: '400',
+          color:'#f5f5f5',
      },
      emailAndPhone:{
-          justifyContent:"space-around",
-          padding:5,
+          flexDirection:'row',
+          justifyContent:"space-between",
+          paddingVertical:5,
+          paddingLeft:5,
+          color:'#f5f5f5',
      },
      email:{
           marginBottom:5,
-          fontSize:18,
-          fontWeight: '200'
+          fontSize:12,
+          fontWeight: '300',
+          color:'#f5f5f5',
      },
      userImage:{
-          width:100,
-          height:100,
-          borderRadius: 100/ 2,
+          width:86,
+          height:86,
+          borderRadius: 86/ 2,
           marginLeft: 8
      }
      
